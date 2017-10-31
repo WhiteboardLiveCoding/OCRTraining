44b66e0017f21c28cacb92191309d30736b907c5
Martin Zlocha <mz4315@ic.ac.uk>
2017-10-31 16:17:49 +0000

    Fix chinese model

--- models/chinese.py
+++ models/chinese.py
@@ -15,23 +15,23 @@ def build(training_data, height=28, width=28):
     input_shape = (height, width, 1)
 
     model = Sequential()
-    model.add(Conv2D(64, (3, 3), weights=[np.random.normal(0, 0.01, size=(3, 3, 1, 64)), np.zeros(64)],
+    model.add(Conv2D(width, (3, 3), weights=[np.random.normal(0, 0.01, size=(3, 3, 1, width)), np.zeros(width)],
                      activation='relu', padding='same', strides=(1, 1),
-                     input_shape=(1, 64, 64)))
+                     input_shape=input_shape))
     model.add(MaxPooling2D(pool_size=(2, 2), strides=(2, 2)))
 
-    model.add(Conv2D(128, (3, 3), weights=[np.random.normal(0, 0.01, size=(3, 3, 64, 128)), np.zeros(128)],
+    model.add(Conv2D(width * 2, (3, 3), weights=[np.random.normal(0, 0.01, size=(3, 3, width, width * 2)), np.zeros(width * 2)],
                      activation='relu', padding='same', strides=(1, 1)))
     model.add(MaxPooling2D(pool_size=(2, 2), strides=(2, 2)))
 
-    model.add(Conv2D(256, (3, 3), weights=[np.random.normal(0, 0.01, size=(3, 3, 128, 256)), np.zeros(256)],
+    model.add(Conv2D(width * 4, (3, 3), weights=[np.random.normal(0, 0.01, size=(3, 3, width * 2, width * 4)), np.zeros(width * 4)],
                      activation='relu', padding='same', strides=(1, 1)))
     model.add(MaxPooling2D(pool_size=(2, 2), strides=(2, 2)))
 
     model.add(Flatten())
 
     model.add(Dropout(0.5))
-    model.add(Dense(1024, activation='relu'))
+    model.add(Dense(width * 8, activation='relu'))
 
     model.add(Dropout(0.5))
     model.add(Dense(nb_classes, activation='softmax'))
